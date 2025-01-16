9e27f0a74bfdc7b1889a2c9d10d6e0baafd0a87f
rmahajan466 <rmahajan466@gmail.com>
2025-01-17 01:39:13 +0530

    Added Animation to Technology Section using framer-motion

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,24 +4,54 @@ import { RiReactjsLine } from "react-icons/ri";
 import { SiMongodb } from "react-icons/si";
 import { FaPython } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+    initial: {y:-10},
+    animate: {
+        y: [10,-10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    },
+});
 
 const Technologies = () => {
     return (
         <div className="border-b border-neutral-800 pb-24">
             <h1 className="my-20 text-center text-4xl">Technologies</h1>
             <div className="flex flex-wrap items-center justify-center gap-4">
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <RiReactjsLine className="text-7xl text-cyan-400" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiMongodb className="text-7xl text-green-500" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaPython className="text-7xl text-cyan-400" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaJava className="text-7xl text-cyan-400" />
-                </div>
+                </motion.div>
             </div>
         </div>
     )
