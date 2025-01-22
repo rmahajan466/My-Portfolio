86fc7c48d74df668f4f2d71b8e071a1bd02d52b5
rmahajan466 <rmahajan466@gmail.com>
2025-01-22 16:00:30 +0530

    Added New Title Logo and new  Projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,10 @@
 import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-02.jpeg";
 import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+import project5 from "../assets/projects/project-5.png";
+import project6 from "../assets/projects/project-6.png";
+import project7 from "../assets/projects/project-7.png";
 
 const CONTENT = `I am an enthusiastic fresher in full stack development, eager to craft robust and scalable web applications. I have built a solid foundation in front-end technologies like React, as well as back-end technologies such as MySQL, and MongoDB. My goal is to apply my growing expertise to create innovative solutions that contribute to business growth and provide exceptional user experiences.`;
 export const HERO_CONTENT = CONTENT.replace(/\. /g, '.\n');
@@ -19,6 +23,15 @@ export const EXPERIENCES = [
 ];
 
 export const PROJECTS = [
+  {
+    title: "My Portfolio",
+    image: project4,
+    description:
+      "Designed and developed my personal portfolio website using React. This project showcases my skills, projects, and experiences in a visually appealing and interactive manner. I implemented responsive design, animations, and smooth transitions to create a seamless user experience.",
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS"],
+    webLink: "https://my-portfolio-chi-sage-27.vercel.app/",
+    gitLink: "https://github.com/rmahajan466/My-Portfolio.git",
+  },
   {
     title: "Tik-Tac-Toe",
     image: project1,
@@ -45,6 +58,30 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "JavaScript"],
     webLink: "https://rmahajan466.github.io/StopWatch/",
     gitLink: "https://github.com/rmahajan466/StopWatch.git",
+  },
+  {
+    title: "Python Hangman Game",
+    image: project5,
+    description: "A classic Hangman game built using Python. This project helped me enhance my Python programming skills, including string manipulation, file handling, and logic implementation. The game randomly selects a word from a predefined list, and the player has to guess the word within a limited number of attempts.",
+    technologies: ["Python"],
+    webLink: "#",
+    gitLink: "https://github.com/rmahajan466/Python_Hangman.git",
+  },
+  {
+    title: "Python Tetris Game",
+    image: project6,
+    description: "A fun Tetris game developed using Python. This project allowed me to explore game development concepts, including game logic, user input, and graphical interfaces. The game features classic Tetris blocks that fall from the top of the screen, and the player must rotate and move the blocks to create horizontal lines and clear the board.",
+    technologies: ["Python"],
+    webLink: "#",
+    gitLink: "https://github.com/rmahajan466/Python-Tetris.git",
+  },
+  {
+    title: "Python Quiz Game",
+    image: project7,
+    description: "This is a simple quiz game developed using Python. The game presents multiple-choice questions to the player, who must select the correct answer from the given options. The project helped me practice Python programming concepts, including functions, loops, conditional statements, requests, JSON, pprint, and random.",
+    technologies: ["Python", "JSON", "API"],
+    webLink: "#",
+    gitLink: "https://github.com/rmahajan466/Quizzing-Game-using-python.git",
   },
 ];
 
