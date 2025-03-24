9cda1dd5e4d81c6a00655117155e53a1d0efbd0e
rmahajan466 <rmahajan466@gmail.com>
2025-03-25 00:19:44 +0530

    Added: New Technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,6 +6,8 @@ import { motion } from "framer-motion";
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3 } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
+import { SiExpress } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
 
 const iconVariants = (duration) => ({
     initial: {y:-10},
@@ -83,6 +85,20 @@ const Technologies = () => {
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiMongodb className="text-7xl text-green-500" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiExpress className="text-7xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaNodeJs className="text-7xl text-green-500" />
                 </motion.div>
             </motion.div>
         </div>
